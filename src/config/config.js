f7c3321baa7ed74038d09cb26d624ca1b4afaa13
Guillermo F Funaro <funaro.g.f@gmail.com>
2023-10-13 09:57:11 -0300

    update

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import "dotenv/config";
 
-const DATABASE_URL = process.env.DATABASE_URL || "";
+const DATABASE_URL = process.env.MONGODB_CONNECT_URI || "";
 
 (async () => {
   try {
