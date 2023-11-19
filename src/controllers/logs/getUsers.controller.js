039fe8b5814ff66b497d55391124100f6f5a7238
Guillermo F Funaro <funaro.g.f@gmail.com>
2023-11-19 08:49:28 -0300

    include populate in response at getUsers

--- src/controllers/logs/getUsers.controller.js
+++ src/controllers/logs/getUsers.controller.js
@@ -2,7 +2,10 @@ import logModel from "../../models/log.model.js";
 
 export const getUsers = async (_req, res) => {
   try {
-    const users = await logModel.find().populate("trainingPlan");
+    const users = await logModel
+      .find()
+      .populate("trainingPlan")
+      .populate("workouts");
     res.json(users);
   } catch (err) {
     res.status(500).json({ message: "Error on the server", error: err });
