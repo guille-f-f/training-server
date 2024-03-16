06fbf18bbb06caa4cc5b77a19bb7a36e539bbd89
Guillermo F Funaro <funaro.g.f@gmail.com>
2024-03-16 14:03:26 -0300

    update

--- src/controllers/auth/sendEmail.controller.js
+++ src/controllers/auth/sendEmail.controller.js
@@ -22,7 +22,12 @@ export const sendEmail = async (req, res) => {
 
     const subject = "Recupero de contraseña";
     const url = `${process.env.FRONTEND_URL}/reset-password/${tokenBase64URL}`;
-    const html = `Para blanquear contraseña siga el siguiente enlace:<a href="${url}/">click aquí</a>`;
+    const html = `<div style="margin: 0; padding: 0; box-sizing: border-box; height: 100vh;">
+                    <h1 style="text-align: center; margin: auto; font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', Segoe UI Symbol, 'Noto Color Emoji'; font-weight: 800; font-size: 4.75rem; color: #191919;">I-RUNNER</h1>
+                    <a href="${url}" style="display: block; margin: auto; text-decoration: none;">
+                      <button style="display: block; margin: auto;background-color: black; color: white; font-weight: 700; padding: 10px 30px 7px; border: none; box-shadow: 0 0 15px -5px white; cursor: pointer;">BLANQUEAR CONTRASEÑA</button>
+                    </a>
+                  </div>`;
 
     sendMailByNodemailer(email, subject, html);
 
